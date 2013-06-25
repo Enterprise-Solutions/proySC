3c62a6746ec042a8d8b6b84e2a640752f8f5c59f
Omar <esgaib@gmail.com>
2013-06-25 17:06:58 -0400

    test

---
+++ Implementacion/proySC/public/js/menu.js
@@ -0,0 +1,138 @@
+var menuPrincipal = [
+		                     {
+			                     codigo: 'org',
+			                     texto: 'Organizacion',
+			                     operaciones: [
+			                                   {
+				                                   nombre: 'Mantener Sede',
+				                                   codigo: 'sede'
+			                                   },
+			                                   {
+				                                   nombre: 'Mantener Tipo Sede',
+				                                   codigo: 'parte'
+			                                   },
+			                                   {
+				                                   nombre: 'Mantener Vendedor',
+				                                   codigo: 'rol-tipo'
+			                                   },
+			                                   {
+				                                   nombre: 'Mantener Proveedor',
+				                                   codigo: 'documento-tipo'
+			                                   },
+			                                   {
+				                                   nombre: 'Mantener Cliente',
+				                                   codigo: 'relacion-tipo'
+			                                   }
+			  			  		 ]
+		                     },
+		                     {
+		                    	 codigo: 'stock',
+			                     texto: 'Stock',
+			                     operaciones:[
+												{
+												    nombre: 'Mantener Articulo Tipo',
+												    codigo: 'articulo'
+												},
+												{
+												    nombre: 'Mantener Articulo',
+												    codigo: 'articulo'
+												},
+												{
+												    nombre: 'Mantener Traslados',
+												    codigo: 'articulo'
+												},
+												{
+												    nombre: 'Mantener Categoria',
+												    codigo: 'articulo'
+												},
+												{
+												    nombre: 'Mantener Marca',
+												    codigo: 'articulo'
+												},
+												{
+												    nombre: 'Mantener Procedencia',
+												    codigo: 'articulo'
+												}
+ 			                     ]
+		                     },
+		                     {
+		                    	 codigo: 'adm',
+			                     texto: 'Administracion',
+			                     operaciones:[
+												{
+												    nombre: 'Mantener Parametros',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Persona',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Mantener Funcionario',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Mantener Persona Tipo',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Mantener Funcionario Tipo',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Mantener Usuario',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Mantener Rol',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Mantener CU',
+												    codigo: 'adm1'
+												},
+												{
+												    nombre: 'Mantener Operacion',
+												    codigo: 'adm1'
+												}
+			                     ]
+		                     },
+		                     {
+		                    	 codigo: 'cont',
+			                     texto: 'Contabilidad',
+			                     operaciones:[
+												{
+												    nombre: 'Mantener Moneda',
+												    codigo: 'moneda'
+												},
+												{
+												    nombre: 'Mantener Tasa Cambio',
+												    codigo: 'tasa-cambio'
+												}
+			                     ]
+		                     },
+		                     {
+		                    	 codigo: 'fact',
+			                     texto: 'Facturacion',
+			                     operaciones:[
+												{
+												    nombre: 'Matener Compra',
+												    codigo: 'compras'
+												},
+												{
+												    nombre: 'Matener Venta',
+												    codigo: 'ventas'
+												}
+			                     ]
+		                     },
+		                     {
+		                    	 codigo: 'rep',
+			                     texto: 'Reportes',
+			                     operaciones:[
+												{
+												    nombre: 'Rep1',
+												    codigo: 'rep1'
+												}
+			                     ]
+		                     }
+		];
