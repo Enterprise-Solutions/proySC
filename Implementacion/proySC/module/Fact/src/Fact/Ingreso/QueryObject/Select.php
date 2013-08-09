473da7c274b45f6c6f788c3824e5d930461843e9
Guido Arce <user@HP-guido>
2013-08-09 16:50:30 -0400

    Busqueda de Facturas actualizada

--- Implementacion/proySC/module/Fact/src/Fact/Ingreso/QueryObject/Select.php
+++ Implementacion/proySC/module/Fact/src/Fact/Ingreso/QueryObject/Select.php
@@ -13,4 +13,36 @@ class Select extends DbSelect
              ->join(array('cm' => 'cont_moneda'), 'fi.cont_moneda_id = cm.cont_moneda_id', array('moneda' => 'nombre', 'moneda_simbolo' => 'simbolo'))
              ->order('fi.doc_fecha');
     }
+    
+    public function addSearchByDocNro($doc_nro)
+    {
+        if ($doc_nro && $doc_nro != "") {
+            $this->_select
+                 ->where("fi.doc_nro ILIKE '%$doc_nro%'");
+        }
+    }
+    
+    public function addSearchByDocFechaDesde($doc_fecha)
+    {
+        if ($doc_fecha && $doc_fecha != "") {
+            $this->_select
+                 ->where("fi.doc_fecha >= '$doc_fecha'");
+        }
+    }
+    
+    public function addSearchByDocFechaHasta($doc_fecha)
+    {
+        if ($doc_fecha && $doc_fecha != "") {
+            $this->_select
+                 ->where("fi.doc_fecha <= '$doc_fecha'");
+        }
+    }
+    
+    public function addSearchByDocTipo($doc_tipo)
+    {
+        if ($doc_tipo && $doc_tipo != "") {
+            $this->_select
+                 ->where("fi.doc_tipo = '$doc_tipo'");
+        }
+    }
 }
