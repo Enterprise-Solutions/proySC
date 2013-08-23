be2185f70bd16fdc0b0000560ed859cef2a59bec
pislas <pabloislas@gmail.com>
2013-08-23 11:48:19 -0400

    Change-Id: I1a4298f7162b75f27e61887e4a9f8879d4d88fac

--- Implementacion/proySC/vendor/EnterpriseSolutions/Controller/SimpleController.php
+++ Implementacion/proySC/vendor/EnterpriseSolutions/Controller/SimpleController.php
@@ -1,18 +1,20 @@
 <?php
+
 namespace EnterpriseSolutions\Controller;
+
 use Zend\Db\TableGateway\Feature;
 use Zend\Db\TableGateway\Feature\MetadataFeature;
 use Zend\Db\TableGateway\TableGateway;
 use Zend\Db\TableGateway\Feature\RowGatewayFeature;
 use Zend\Db\RowGateway\RowGateway;
 use Zend\Db\Adapter\Adapter;
-class SimpleController  extends BaseController
+
+class SimpleController extends BaseController
 {
 	public $_tableGateway;
 	public $_tableName;
 	public $_pkName;
 	
-
 	public function indexAction()
 	{
 		$rs = $this->_getTableGateway()->select();
@@ -26,15 +28,12 @@ class SimpleController  extends BaseController
 		$pkName = $this->_getTablePrimaryKey();
 		$this->_getTableGateway()->insert($postData);
 		$seq = $this->_getPkSequenceName();
+		
 		$dbAdapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
 		$rs = $dbAdapter->query("select currval('$seq') as id", Adapter::QUERY_MODE_EXECUTE)->toArray();
 		$lastInsertedValue = $rs[0]['id'];
 		$postData[$pkName] = $lastInsertedValue;
 		return $this->_returnAsJson($postData);
-		/*$rowGateway = new RowGateway($pkName,$this->_tableName,$this->getServiceLocator()->get('Zend\Db\Adapter\Adapter'));
-		$rowGateway->populate($postData);
-		$rowGateway->save();
-		return $this->_returnAsJson($rowGateway->toArray());*/
 	}
 	
 	public function putAction()
@@ -81,14 +80,6 @@ class SimpleController  extends BaseController
 	
 	public function _getTablePrimaryKey()
 	{
-		//$metadata = $this->_getTableGateway()->featureSet->getFeatureByClassName('Zend\Db\TableGateway\Feature\MetadataFeature');
-		//$metadata = $this->_getTableGateway()->getFeatureSet()->getFeatureByClassName('Zend\Db\TableGateway\Feature\MetadataFeature');
-		/*$featureSet = $this->_getTableGateway()->getFeatureSet();
-		$metadata = $featureSet->getFeatureByClassName('Zend\Db\TableGateway\Feature\MetadataFeature');
-		
-		$metadata->postInitialize();
-		$primaryKey = $metadata->sharedData['metadata']['primaryKey'];
-		return $primaryKey;*/
 		if(!$this->_pkName){
 			$this->_pkName = $this->_tableName . "_id";
 		}
