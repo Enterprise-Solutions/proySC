08ca97b73e8039c43deca78bbf1e6dfb09711fc1
Omar <esgaib@gmail.com>
2013-08-29 14:47:11 -0400

    stores varios

--- Implementacion/proySC/public/js/stores/factVentaDetalleStore.js
+++ Implementacion/proySC/public/js/stores/factVentaDetalleStore.js
@@ -1,4 +1,4 @@
-var factCompraDetalleStore = new Ext.data.JsonStore({
+var factVentaDetalleStore = new Ext.data.JsonStore({
 	// store configs
 	totalProperty: 'numResults',
 	//url: baseURL + '/stock/articulo/index',
@@ -6,5 +6,5 @@ var factCompraDetalleStore = new Ext.data.JsonStore({
 	root: '',
 	//autoLoad:true,
 	idProperty: 'stock_articulo_id',//no pueden haber dos articulos igfuales en la misma compra
-	fields: ['fact_ingreso_detalle_id','fact_ingreso_id','stock_articulo_id','cantidad','costo_unit','articulo','porc_impuesto']
+	fields: ['fact_egreso_detalle_id','fact_egreso_id','precio_venta_final','stock_articulo_id','cantidad','precio_unit','articulo','porc_impuesto']
 });
